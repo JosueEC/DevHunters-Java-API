5fac59b0d998178caeea7e12c87eb829b45c995f
J Evangelista Cruz <josueev.cruz@gmail.com>
2023-08-12 17:04:01 -0600

    Start of the project

---
+++ src/test/java/com/apirestmvc/devhunters/DevhuntersapijavaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.apirestmvc.devhunters;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DevhuntersapijavaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
