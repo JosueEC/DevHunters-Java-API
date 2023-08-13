52d5653da22f0e5d4278fa309a2f1ae3fa0abda5
J Evangelista Cruz <josueev.cruz@gmail.com>
2023-08-13 11:04:55 -0600

    build(services): ➕ Add Spring Security to the API

--- src/main/java/com/apirestmvc/devhunters/User/Controllers/UserController.java
+++ src/main/java/com/apirestmvc/devhunters/User/Controllers/UserController.java
@@ -2,7 +2,6 @@ package com.apirestmvc.devhunters.User.Controllers;
 
 import com.apirestmvc.devhunters.User.Models.User;
 import com.apirestmvc.devhunters.User.Services.UserService;
-import org.apache.coyote.Response;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
