bc2d7797371b52fc0423d7cb6923ba5881267b1a
J Evangelista Cruz <josueev.cruz@gmail.com>
2023-08-20 15:04:04 -0600

    feat(models): 🔨 Extends UserModel from UserDetails interface

--- src/main/java/com/apirestmvc/devhunters/Demo/User/models/User.java
+++ src/main/java/com/apirestmvc/devhunters/Demo/User/models/User.java
@@ -6,26 +6,81 @@ import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
 import lombok.NoArgsConstructor;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
 
-@Entity
-@Table(name = "users")
+import java.util.Collection;
+
+@SuppressWarnings("ALL")
 @Data
 @Builder
 @NoArgsConstructor
 @AllArgsConstructor
-public class User {
+@Entity
+@Table(name = "users")
+public class User implements UserDetails {
 	@Id
 	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	private Long id;
-	@Column(name = "name")
+	@Column(
+			name = "name",
+			nullable = false
+	)
 	private String name;
 	@Column(
 			name = "email",
-			unique = true
+			unique = true,
+			nullable = false
 	)
 	private String email;
-	@Column(name = "password")
+	@Column(
+			name = "password",
+			nullable = false
+	)
 	private String password;
-	@Column(name = "role")
+	@Column(
+			name = "role",
+			nullable = false
+	)
+	/*
+	 Esta anotation le indica a spring boot que esta columna
+	 es un enum de tipo string
+	*/
+	@Enumerated(EnumType.STRING)
 	Role role;
+	
+	@Override
+	public Collection<? extends GrantedAuthority> getAuthorities() {
+		return null;
+	}
+	
+	@Override
+	public String getUsername() {
+		return null;
+	}
+	
+	/*
+	 Los metodos que devuelven un boolean se colocan en true, debido
+	 a que estas verificaciones van a ser controlada en el service de
+	 JWT
+	*/
+	@Override
+	public boolean isAccountNonExpired() {
+		return true;
+	}
+	
+	@Override
+	public boolean isAccountNonLocked() {
+		return true;
+	}
+	
+	@Override
+	public boolean isCredentialsNonExpired() {
+		return true;
+	}
+	
+	@Override
+	public boolean isEnabled() {
+		return true;
+	}
 }
